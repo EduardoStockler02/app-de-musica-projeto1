6f5ef1bf7c46fc9e46986bf52d659197f56516a9
Eduardo Stockler <stocklerdudu@gmail.com>
2025-02-20 15:45:08 -0300

    ajustes no js e no css

--- main.js
+++ main.js
@@ -1,53 +1,50 @@
 // Seleciona os elementos do DOM
-const botaoMusica = document .getE1ementById( 'play_btn ' ) ;
-const musica = new Audio( ' som/song. mp3 ' ) ;
+const botaoMusica = document.getElementById('play_btn');
+const musica = new Audio('som/song.mp3');
 
-let nextDom = document . getE1ementById( 'next' ) ;
-let prevDom = document . getE1ementById( 'prev' ) ;
+let nextDom = document.getElementById('next');
+let prevDom = document.getElementById('prev');
 
-let carouselDom = document. querySe1ector( '.carousel' ) ;
-let SliderDom = carouselDom.querySe1ector('.carousel .list' ) ;
-
+let carouselDom = document.querySelector('.carousel');
+let SliderDom = carouselDom.querySelector('.carousel .list');
 
 // Seleciona os elementos do carrossel
-let thumbnailBorderDom = document . querySe1ector('.carousel .thumbnail');
-let thumbnailItemsDom = thumbnailBorderDom.querySe1ectorA11('.item');
-let timeDom = document . querySe1ector('.carousel .time');
+let thumbnailBorderDom = document.querySelector('.carousel .thumbnail');
+let thumbnailItemsDom = thumbnailBorderDom.querySelectorAll('.item');
+let timeDom = document.querySelector('.carousel .time');
 
-// Adiciona o primeiro item do carrossel ao final da lista para criar a animacäo de loop
+// Adiciona o primeiro item do carrossel ao final da lista para criar a animação de loop
 thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
 
-// Define o tempo de duracäo da animacäo e o tempo de espera para o pr6ximo slide automético
-let timeRunning = 3000; // Tempo da animacäo (em milissegundos)
-let timeAutonext = 9000; // Tempo de espera para o pr6ximo slide automético (em milissegundos)
+// Define o tempo de duração da animação e o tempo de espera para o próximo slide automático
+let timeRunning = 3000; // Tempo da animação (em milissegundos)
+let timeAutonext = 9000; // Tempo de espera para o próximo slide automático (em milissegundos)
 
-
-botaoMusica . addEventListener( " click", () => {
-    if(musica.paused){
+botaoMusica.addEventListener("click", () => {
+    if (musica.paused) {
         musica.play();
-    }
-    else{
+    } else {
         musica.pause();
     }
 });
 
-nextDom.onc1ick = function () {
-    showS1ider('next');
-}
-prevDom.onc1ick = function () {
-    showS1ider('prev');
-}
+nextDom.onclick = function () {
+    showSlider('next');
+};
+prevDom.onclick = function () {
+    showSlider('prev');
+};
 
 let runTimeOut;
 let runNextAuto = setTimeout(() => {
     nextDom.click();
 }, timeAutonext);
 
-function showS1ider(type) {
-    let SliderItemsDom = SliderDom.querySe1ectorA11('.carousel .list .item') ;
-    let thumbnailItemsDom = document.querySe1ectorA11('.carousel .thumbnail .item') ;
+function showSlider(type) {
+    let SliderItemsDom = SliderDom.querySelectorAll('.carousel .list .item');
+    let thumbnailItemsDom = document.querySelectorAll('.carousel .thumbnail .item');
     
-    if(type === "next"){
+    if (type === "next") {
         SliderDom.appendChild(SliderItemsDom[0]);
         thumbnailBorderDom.appendChild(thumbnailItemsDom[0]);
         carouselDom.classList.add('next');
@@ -63,9 +60,7 @@ function showS1ider(type) {
     }, timeRunning);
 
     clearTimeout(runNextAuto);
-
-    runNextAuto = setTimeout(() =>{
+    runNextAuto = setTimeout(() => {
         nextDom.click();
     }, timeAutonext);
 }
-
